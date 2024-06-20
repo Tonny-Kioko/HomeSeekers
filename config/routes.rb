b902e1f2a328bc7a5d31a2964c44b4be5354f002
Tonny-Kioko <cfakiokoanthony@gmail.com>
2024-06-20 10:41:58 +0300

    setting up database files and settings for postres

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :homes
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  root to: "homes#index" 
+  
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  # root "homes#index" # This is commented out since it's already defined above
 end
