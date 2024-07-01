850beaa5f6cc2394a9ba6cc39e4c123172fc90c2
Tonny-Kioko <cfakiokoanthony@gmail.com>
2024-07-01 14:37:52 +0300

    Feature: Adding reviews for each product

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
-  belongs_to :property
+  belongs_to :property, counter_cache: true
 
 
   validates :content, presence: true
@@ -12,6 +12,7 @@ class Review < ApplicationRecord
   validates :value_rating, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
 
   after_commit :update_final_rating, on[:create, :update]
+  after_commit :update_average_final_rating, on[:create, :update]
 
   def update_final_rating
     total_points = (
@@ -24,6 +25,9 @@ class Review < ApplicationRecord
       )
 
     update_column(:final_rating, total_points.to_f / 6)
+
+    property.update_average_rating
   end
+
 
 end
