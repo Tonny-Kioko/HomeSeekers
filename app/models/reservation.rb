d7e0546218032538d94aa90e7caf08d61702091e
Tonny-Kioko <cfakiokoanthony@gmail.com>
2024-07-11 13:03:09 +0300

    Feature: building the reservation model feature and working on all its logic to ensure it works well

---
+++ app/models/reservation.rb
@@ -0,0 +1,12 @@
+class Reservation < ApplicationRecord
+  belongs_to :user
+  belongs_to :property
+
+  validates :checkin_date, presence: true
+  validates :checkout_date, presence: true
+
+  scope :upcoming_reservations, -> { where("checkin_date > ?", Date.today).order(:checkin_date) }
+  scope :current_reservations, -> { where("checkout_date > ? AND checkin_date < ?", Date.today, Date.today).order(:checkout_date) }
+
+  # Ex:- scope :active, -> { where(active: true) }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_10_092057) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_11_082120) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,6 +64,18 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_10_092057) do
     t.decimal "average_final_rating"
   end
 
+  create_table "reservations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "property_id", null: false
+    t.date "checkin_date"
+    t.date "checkout_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["property_id"], name: "index_reservations_on_property_id"
+    t.index ["user_id", "property_id", "checkin_date", "checkout_date"], name: "idx_on_user_id_property_id_checkin_date_checkout_da_7282e2dfd7", unique: true
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
+
   create_table "reviews", force: :cascade do |t|
     t.string "content"
     t.integer "cleanliness_rating"
@@ -105,6 +117,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_10_092057) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "reservations", "properties"
+  add_foreign_key "reservations", "users"
   add_foreign_key "reviews", "properties"
   add_foreign_key "reviews", "users"
   add_foreign_key "wishlists", "properties"
