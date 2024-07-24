6f9e67fa023eb5e7c3b048e7892da14fb784e3f7
Tonny-Kioko <cfakiokoanthony@gmail.com>
2024-07-24 11:44:04 +0300

    Feature: buiilding the amenities feature and attaching it to individual properties ion a many to many relationship

--- app/models/property.rb
+++ app/models/property.rb
@@ -23,6 +23,9 @@ class Property < ApplicationRecord
 
   has_rich_text :description
 
+  has_many :property_amenities, dependent: :destroy
+  has_many :amenities, through: :property_amenitis, source: :amenity, dependent: :destroy
+
   def update_average_rating
     average_rating = reviews.average(:final_rating);
     update_column(:average_final_rating, average_rating)
